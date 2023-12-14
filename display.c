8b4026eeb68d37797350b495933a3ebfcb1a4bd2
Nazirite <nnazirite@gmail.com>
2023-12-14 00:53:55 -0800

    simple shell

---
+++ display.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * display - prints dollar sign
+ *
+ * Return: void
+ */
+
+void display(void)
+{
+print_sign("$ ");
+}
---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* Includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* Prototypes */
+
+void display(void);
+void print_sign(const char *message);
+void execute_command(const char *line);
+void read_command(char *line, size_t len);
+
+
+
+#endif
